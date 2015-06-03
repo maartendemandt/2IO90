32e72204e87f3eb76a93c200d2926d43cb6c19c0
Martin <martin.ter.haak@gmail.com>
2015-06-03 10:14:35 +0200

    Revert "Cleaned output and added more data"
    This reverts commit e417efaa9f477008eb05fd17ef185300146b8285.

--- src/some/pack/age/algorithm/AnnealingAlgorithm.java
+++ src/some/pack/age/algorithm/AnnealingAlgorithm.java
@@ -59,7 +59,7 @@ public class AnnealingAlgorithm implements IAlgorithm
             s.bumpPhase();
         }
         //s.kill();
-        System.out.println("Max quality: "+maxQuality);
+        System.out.println("max quality: "+maxQuality);
         return bestSolution;
     }
 
